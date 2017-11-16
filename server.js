e66fc3a99865172666677d127338d9390df9b3e1
Александр Горбатько <avgorbatko@chel.rusoft.local>
2017-11-16 16:52:22 +0500

    added express

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ var Stomp = require('stomp-client');
 
 var stomp = new Stomp('172.27.30.61', 61613, 'artemis', 'simetraehcapa');
 
-stomp.connect(function(sId) {
-  stomp.subscribe('jms.topic.application_state', {"heart-beat": 20000}, function(body, headers) { 
-    console.log(body, headers);
-  }); 
-});
+var connect = function() {
+	stomp.connect(function(sId) {
+		stomp.subscribe('jms.topic.application_state', function(body, headers) { 
+			console.log(body, headers);
+		}); 
+	});
+};
 
 process.on('SIGINT', function() {
-    stomp.disconnect(function() {
-      console.log('DISCONNECTED');
-    });
+	stomp.disconnect(function() {
+		console.log('DISCONNECTED');
+	});
 });
